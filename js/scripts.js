974d53002b8523146de3508e74b3aed78a7eb747
Maureen Hidalgo <mchidalgo12@gmail.com>
2020-03-09 20:35:43 -0700

    update user interface logic to fill the spans in HTML based on the value of result

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// business logic
 var leapYear = function(year) {
   if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
@@ -6,13 +7,21 @@ var leapYear = function(year) {
   }
 };
 
-// jQuery user interface logic here.
-
+// user interface logic
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+
+    $(".year").text(year);
+
+    if (!result) {                 // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
   });
 });
